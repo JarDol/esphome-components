3f6474168b6a430c9c0b692acba4c9d3bbf91072
JarDol <104271327+JarDol@users.noreply.github.com>
2026-09-15 22:34:21 +0200

    Fix typo in reactive power field names

--- components/wmbus_common/driver_amiplus.cpp
+++ components/wmbus_common/driver_amiplus.cpp
@@ -241,7 +241,7 @@ namespace
         );
 
         addNumericFieldWithExtractor(
-            "reactive_power_inductive",
+            "reactive_power_inductiver",
             "Reactive power (inductive).",
             DEFAULT_PRINT_PROPERTIES,
             Quantity::Reactive_Power,
@@ -263,7 +263,7 @@ namespace
         );
 
         addNumericFieldWithExtractor(
-            "reactive_power_capacitive",
+            "reactive_power_capacitiver",
             "Reactive power (capacitive).",
             DEFAULT_PRINT_PROPERTIES,
             Quantity::Reactive_Power,
