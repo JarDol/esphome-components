7744d3dba936a2ed9c0e8c5b44c4e25634146fae
JarDol <104271327+JarDol@users.noreply.github.com>
2026-09-15 20:07:25 +0000

    Fix Amiplus reactive power scaling

--- components/wmbus_common/driver_amiplus.cpp
+++ components/wmbus_common/driver_amiplus.cpp
@@ -245,10 +245,11 @@ namespace
             "Reactive power (inductive).",
             DEFAULT_PRINT_PROPERTIES,
             Quantity::Reactive_Power,
-            VifScaling::Auto, DifSignedness::Signed,
+            VifScaling::None, DifSignedness::Signed,
             FieldMatcher::build()
             .set(DifVifKey("0BFB14")),
-            Unit::KVAR
+            Unit::KVAR,
+            0.001
         );
 
         addNumericFieldWithExtractor(
@@ -267,10 +268,11 @@ namespace
             "Reactive power (capacitive).",
             DEFAULT_PRINT_PROPERTIES,
             Quantity::Reactive_Power,
-            VifScaling::Auto, DifSignedness::Signed,
+            VifScaling::None, DifSignedness::Signed,
             FieldMatcher::build()
             .set(DifVifKey("0BFB943C")),
-            Unit::KVAR
+            Unit::KVAR,
+            0.001
         );
 
         addNumericFieldWithExtractor(
